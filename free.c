40c0494e2449989678e37d8969a97357503c7929
ManNjoro <elijohnmwoho@gmail.com>
2023-05-24 17:42:02 -0700

    corrections

--- free.c
+++ free.c
@@ -15,3 +15,39 @@ void free_stack(stack_t *head)
 		head = temp;
 	}
 }
+
+/**
+ * get_tokens - get tokens from line
+ * @line: line from file
+ * @line_number: line number
+ *
+ * Return: int
+ */
+
+char *get_tokens(char *line, unsigned int line_number)
+{
+	char delim[2] = "\n ";
+	char *token = NULL;
+	char *holder = NULL;
+
+	token = strtok(line, delim);
+	if (token == NULL)
+		return (NULL);
+	holder = strtok(NULL, delim);
+	if (holder != NULL)
+	{
+		if (is_number(holder))
+			global_var = atoi(holder);
+		else
+		{
+			printf("L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (holder == NULL && strcmp(token, "push") == 0)
+	{
+		printf("L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	return (token);
+}
