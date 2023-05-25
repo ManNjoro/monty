3364cb1716c95f22f64c1b315d373a614edc8780
ManNjoro <elijohnmwoho@gmail.com>
2023-05-24 18:30:37 -0700

    nop

---
+++ more_ops.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop_func - does nothing
+ * @stack: pointer
+ * @line_number: line number
+ * Return: void
+ */
+void nop_func(stack_t **n, unsigned int line_n)
+{
+	(void) n;
+	(void) line_n;
+}
